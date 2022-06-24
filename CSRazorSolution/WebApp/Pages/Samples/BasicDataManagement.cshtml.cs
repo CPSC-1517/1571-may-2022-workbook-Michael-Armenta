ce38eea279a5236150f9bcb6450ac139b3c1e095
Michael-Armenta <m.armenta271@gmail.com>
2022-06-24 17:10:17 -0600

    Add images for classroom demo

--- CSRazorSolution/WebApp/Pages/Samples/BasicDataManagement.cshtml.cs
+++ CSRazorSolution/WebApp/Pages/Samples/BasicDataManagement.cshtml.cs
@@ -5,8 +5,40 @@ namespace WebApp.Pages.Samples
 {
     public class BasicDataManagementModel : PageModel
     {
+        // BindProperty is an annotation that handles two-way data transfer
+        // two-way? means output to form for display AND input from form for processing
+        [BindProperty]
+        public int Num { get; set; }
+        [BindProperty]
+        public string MassText { get; set; }
+        [BindProperty]
+        public int FavouriteCourse { get; set; }
+
+        // Feedback is just for display, doesn't need a BindProperty on it.
+        public string Feedback { get; set; }
         public void OnGet()
         {
+            if(Num == 0)
+            {
+                Feedback = "executing the OnGet method for the Get request";
+            }
+            else
+            {
+                Feedback = $"You entered the value {Num} displayed by OnGet";
+            }
+        }
+
+        public void OnPost()
+        {
+            if (Num == 0)
+            {
+                Feedback = "executing the OnPost method for a value of zero";
+            }
+            else
+            {
+                Feedback = $"You entered the value {Num} (displayed by OnPost)" +
+                $" your mass text is {MassText} and I like the {FavouriteCourse} course";
+            }
         }
     }
 }
